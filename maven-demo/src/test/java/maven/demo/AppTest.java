294ff2186f07c057c66992d0716e7fc34fce0eef
Srikanth Ghattamaneni <ghattamanenisrikanth8@gmail.com>
2022-03-21 10:48:22 +0530

    maven

---
+++ maven-demo/src/test/java/maven/demo/AppTest.java
@@ -0,0 +1,10 @@
+package maven.demo;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+}
+    
---
+++ maven-demo/src/test/java/maven/demo/DemoTest.java
@@ -0,0 +1,25 @@
+package maven.demo;
+
+//import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+	@Test
+	public void testcase() {
+		assertEquals(10,10);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		int x=5;
+		int y=7;
+		//System.out.println(2+3+"hello"+((y*2)%x));
+	
+
+}
+	
+	
+}
